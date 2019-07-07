7f5f17dc1be1ab49fec9fdcccbdce2f385c64718
Valron <charliescenek@gmail.com>
2019-07-07 21:33:57 +0300

    Added newUser router

---
+++ backend/routes/newUser.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+let User = require("../models/userModel");
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json("Error:" + err));
+});
+
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const age = req.body.age;
+  const password = req.body.password;
+  const name = req.body.name;
+  const email = req.body.email;
+  const image = req.body.image;
+
+  const newUser = new User({
+    username,
+    password,
+    name,
+    age,
+    email,
+    image
+  });
+
+  newUser
+    .save()
+    .then(() => req.json("User added!"))
+    .catch(err => res.status(400).json("Error:" + err));
+});
+
+module.exports = router;
